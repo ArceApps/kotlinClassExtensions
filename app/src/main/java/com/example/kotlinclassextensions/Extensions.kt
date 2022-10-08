a1d43767dbc6d84a63e10b15e45de610f50b8118
ArceApps <arce.com.apps@gmail.com>
2022-10-08 22:49:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kotlinclassextensions/Extensions.kt
@@ -0,0 +1,53 @@
+package com.example.kotlinclassextensions
+
+import android.app.Activity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Created by Oscar Arce
+ * on 03/10/2022.
+ */
+class Extensions {
+
+    fun Any?.isNull() : Boolean {
+        return this == null
+    }
+
+    fun String.removeFirstLastChar() : String {
+         return this.substring(1, this.length -1)
+    }
+
+    fun String.countSpaces(): Int {
+        return this.count { count -> count == ' ' }
+    }
+
+    fun Activity.showToast(text : String, duration : Int = Toast.LENGTH_LONG) {
+        Toast.makeText(this, text, duration).show()
+    }
+
+    fun Activity.color(@ColorRes color : Int) {
+        ContextCompat.getColor(this, color)
+    }
+
+    fun EditText.onTextChanged(listener : (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                //Se ha añadido un nuevo caracter al edit text
+                listener(p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Se va a añadir un nuevo caracter al edit text
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Ya se ve el caracter en el edit text
+            }
+        })
+    }
+}
